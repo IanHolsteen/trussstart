caa2c137d29f08b23c2f86c937647715fd6d0d4d
IanHolsteen <ianholsteen@gmail.com>
2025-04-29 23:25:27 -0400

    yu

--- client/app/designer_profiles/SwipeableProfiles.js
+++ client/app/designer_profiles/SwipeableProfiles.js
@@ -20,8 +20,8 @@ export default function SwipeableProfiles({ profiles }) {
   // const API_BASE_URL = 'http://localhost:3000'
   if (!current) return <div className="text-center mt-20">No more profiles 💤</div>
 
-  const photoUrl = `/api/${current.photo_url}`
-  const coverUrl = `/api/${current.cover_photo_url}`
+  const photoUrl = `/${current.photo_url}`
+  const coverUrl = `/${current.cover_photo_url}`
 
   return (
     <div className="flex flex-col items-center justify-between p-6">
